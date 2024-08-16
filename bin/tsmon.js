9cb126d22b8b602a140d9eebf410bc78b876bfd3
e2tox <e2tox@live.com>
2024-08-16 11:30:15 +0800

    build with tsb

--- bin/tsmon.js
+++ bin/tsmon.js
@@ -40,7 +40,7 @@ this.report("info","(1/3) Using: "+this.configFile),!this.tsc.findConfigFile(__d
 ;this.sys=Object.assign({},this.tsc.sys,{writeFile:(t,e)=>{const s=t.slice(0,t.length-3)+".ts";this.files.set(s,e),
 this.report("start","(3/3) Building..."+s)}}),this.setupRestartCommand()}setupRestartCommand(){
 process.stdin.setEncoding("utf8"),process.stdin.on("readable",(()=>{let t;for(;null!=(t=process.stdin.read());){
-const e=(""+t).trim()
+const e=`${t}`.trim()
 ;"rs"===e?this.start():"kill"===e?this.thread&&this.thread.initialized?this.kill():console.log(i.Yellow("[ Unregistered ]"),i.White("tsmon: no process to kill")):"debug"===e&&(this.thread&&this.thread.initialized?this.thread.kill("SIGUSR1"):console.log(i.Yellow("[ Unregistered ]"),i.White("tsmon: no process to debug")))
 }})),process.stdin.on("end",(()=>{process.stdout.write("Console input stream end")}))}createReporter(){
 if(this.reportFile)return new Thread(this.reportFile,this.entryArgv,{execArgv:this.execArgv,cwd:process.cwd(),
@@ -65,12 +65,12 @@ function d(t){try{return e.statSync(t).isFile()}catch(t){return!1}}function g(t)
 function m(t){return t.substring(0,t.lastIndexOf("."))||t}function R(t){const e=t.indexOf("*")
 ;return t.substr(0,e).length}function k(e){
 return"index"===e.type?t.dirname(e.path):"file"===e.type?e.path:"extension"===e.type?m(e.path):"package"===e.type?e.path:y(e.type)
-}function y(t){throw new Error("Unknown type "+t)}function v(t,e){if(e.length<t.length)return;if("*"===t)return e
+}function y(t){throw new Error(`Unknown type ${t}`)}function v(t,e){if(e.length<t.length)return;if("*"===t)return e
 ;const s=t.indexOf("*");if(-1===s)return;const i=t.substring(0,s),r=t.substring(s+1)
 ;return e.substr(0,s)===i&&e.substr(e.length-r.length)===r?e.substr(s,e.length-r.length):void 0}
-function j(e,s,i=["main"],r=!0){const n=function(e,s,i){const r=Object.keys(s).concat().sort(((t,e)=>R(e)-R(t))),n=[]
+function $(e,s,i=["main"],r=!0){const n=function(e,s,i){const r=Object.keys(s).concat().sort(((t,e)=>R(e)-R(t))),n=[]
 ;for(const i of r)n.push({pattern:i,paths:s[i].map((s=>t.join(e,s)))});return!s["*"]&&i&&n.push({pattern:"*",
-paths:[e.replace(/\/$/,"")+"/*"]}),n}(e,s,r)
+paths:[`${e.replace(/\/$/,"")}/*`]}),n}(e,s,r)
 ;return(t,e,s,r)=>function(t,e,s=g,i=d,r=Object.keys(require.extensions),n=["main"]){const o=function(t,e,s){
 if(!e||!s||"."===s[0]||s[0]===u.default.sep)return;const i=[];for(const r of e){const e=r.pattern===s?"":v(r.pattern,s)
 ;if(void 0!==e)for(const s of r.paths){const r=s.replace("*",e);i.push({type:"file",path:r}),i.push(...t.map((t=>({
@@ -78,16 +78,16 @@ type:"extension",path:r+t})))),i.push({type:"package",path:u.default.join(r,"pac
 ;const n=u.default.join(r,"index");i.push(...t.map((t=>({type:"index",path:n+t}))))}}return 0===i.length?void 0:i
 }(r,t,e);if(o)return function(t,e=g,s,i=["main"]){
 for(const r of t)if("file"===r.type||"extension"===r.type||"index"===r.type){if(s(r.path))return k(r)
-}else if("package"===r.type){const t=e(r.path);if(t){const e=w(t,i,r.path,s);if(e)return m(e)}}else y(r.type)}(o,s,i,n)
-}(n,t,e,s,r,i)}function w(t,e,s,i){for(let r=0;r<e.length;r++){const n=t[e[r]];if(n&&"string"==typeof n){
-const t=a.join(a.dirname(s),n);if(i(t))return t}}}function $(t,e){return s=>{
-const i=s.getCompilerOptions(),r=x(i.baseUrl)?a.join(t.inputDir,i.baseUrl||"."):i.baseUrl||".",n=[".ts",".js",".json",".node"],o=j(r,i.paths||{})
+}else if("package"===r.type){const t=e(r.path);if(t){const e=j(t,i,r.path,s);if(e)return m(e)}}else y(r.type)}(o,s,i,n)
+}(n,t,e,s,r,i)}function j(t,e,s,i){for(let r=0;r<e.length;r++){const n=t[e[r]];if(n&&"string"==typeof n){
+const t=a.join(a.dirname(s),n);if(i(t))return t}}}function w(t,e){return s=>{
+const i=s.getCompilerOptions(),r=x(i.baseUrl)?a.join(t.inputDir,i.baseUrl||"."):i.baseUrl||".",n=[".ts",".js",".json",".node"],o=$(r,i.paths||{})
 ;return t=>function(t,e,s,i,r,n){
 const o=t,{isExportDeclaration:c,isImportDeclaration:u,visitEachChild:h,visitNode:l}=n,{factory:p}=e
 ;return l(o,(t=>f(t))),t;function f(t){return null==t?[]:c(t)||u(t)?function(t){return!t.moduleSpecifier||function(t){
 return!(!t.moduleSpecifier||!t.moduleSpecifier.getSourceFile())&&g(d(t.moduleSpecifier))}(t)?t:function(t,e){
 if(!t.moduleSpecifier||!t.moduleSpecifier.getSourceFile())return t;const s=d(t.moduleSpecifier),n=i(s,void 0,void 0,r)
-;if(!n)return t;const o=a.relative(e,n).replace(/\\/g,"/"),h=g(o)?o:"./"+o
+;if(!n)return t;const o=a.relative(e,n).replace(/\\/g,"/"),h=g(o)?o:`./${o}`
 ;return u(t)?p.updateImportDeclaration(t,t.modifiers,t.importClause,p.createStringLiteral(h),t.attributes):c(t)?p.updateExportDeclaration(t,t.modifiers,t.isTypeOnly,t.exportClause,p.createStringLiteral(h),t.attributes):t
 }(t,a.dirname(o.fileName))}(t):h(t,(t=>f(t)),e)}function d(t){
 return t.getText().substr(t.getLeadingTriviaWidth(),t.getWidth()-2*t.getLeadingTriviaWidth())}function g(t){
@@ -99,21 +99,21 @@ this.tsc=s;const n={getCanonicalFileName:t=>t,getCurrentDirectory:s.sys.getCurre
 sourceMap:!1,inlineSourceMap:!0,incremental:!0,declaration:!1,declarationMap:!1,tsBuildInfoFile:r.cacheFile
 },r.sys,o,(t=>{c++,console.error("\r\n"+s.formatDiagnosticsWithColorAndContext([t],n))}),(t=>{
 if(6031===t.code&&console.log(),6032===t.code&&r.stop(),6193===t.code||6194===t.code){let t=c;if(c=0,t){
-const e=t>1?t+" errors":"1 error";r.options.ignoreErrors?(r.report("info","(3/3) Building...ignored "+e),
+const e=t>1?`${t} errors`:"1 error";r.options.ignoreErrors?(r.report("info","(3/3) Building...ignored "+e),
 setTimeout((()=>{r.start()}),5)):(r.report("info","(3/3) Building...found "+e),setTimeout((()=>{
 console.log(i.Yellow("[ Unregistered ]"),i.White("tsmon: modify source code or enter 'rs' to run"))}),5))
 }else r.report("info","(3/3) Building...success"),setTimeout((()=>{r.start()}),5)}})),a=u.watchFile
-;u.watchFile=(t,e,s)=>a(t,((t,s)=>(console.log(i.Yellow("[ Unregistered ]"),i.Yellow("tsmon: file changed: "+t)),
+;u.watchFile=(t,e,s)=>a(t,((t,s)=>(console.log(i.Yellow("[ Unregistered ]"),i.Yellow(`tsmon: file changed: ${t}`)),
 e(t,s))),s);const h=u.createProgram;u.createProgram=(...t)=>{r.report("start","(2/3) Initializing...")
 ;const e=Reflect.apply(h,u,t);return r.report("info","(2/3) Initializing...done"),e},u.useCaseSensitiveFileNames=()=>!0
 ;const l=u.afterProgramCreate;u.afterProgramCreate=t=>{r.report("start","(3/3) Building...")
-;const e=t.getCompilerOptions();if(e.baseUrl&&e.paths&&Object.keys(e.paths)){const e={};e.before=[$({
+;const e=t.getCompilerOptions();if(e.baseUrl&&e.paths&&Object.keys(e.paths)){const e={};e.before=[w({
 inputDir:r.configDir},s)];const i=t.emit;t.emit=function(s,r,n,o,c){return c?((c.before||[]).concat(e.before||[]),
 (c.after||[]).concat(e.after||[]),(c.afterDeclarations||[]).concat(e.afterDeclarations||[]),
 Reflect.apply(i,t,[s,r,n,o,c])):Reflect.apply(i,t,[s,r,n,o,e])}}l(t)};const p=require(u.configFileName)
 ;if(Array.isArray(p.watch)&&this.tsc.sys.watchDirectory){const s=t.dirname(u.configFileName);for(const n of p.watch){
 const o=t.join(s,n);e.watch(o,((e,n)=>{if(n){const e=t.join(s,n)
-;console.log(i.Yellow("[ Unregistered ]"),i.Yellow("tsmon: watching folder changed: "+e)),r.start()}}))}}}watch(){
+;console.log(i.Yellow("[ Unregistered ]"),i.Yellow(`tsmon: watching folder changed: ${e}`)),r.start()}}))}}}watch(){
 this.tsc.createWatchProgram(this.hostOfConfigFile)}}!function(){
 const s=PackageFileResolver.FromName("typescript",__dirname)
 ;if(!s)return void console.log(i.Red("ERROR: No typescript runtime found. exiting..."))
